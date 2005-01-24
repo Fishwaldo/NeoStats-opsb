c27475cb94ed58bd1bd98810c267af4d1d268702
Mark <>
2005-01-24 22:22:57 +0000

    update copyright year

--- opsb.c
+++ opsb.c
@@ -1,5 +1,5 @@
 /* NeoStats - IRC Statistical Services 
-** Copyright (c) 1999-2004 Adam Rutter, Justin Hammond, Mark Hetherington
+** Copyright (c) 1999-2005 Adam Rutter, Justin Hammond, Mark Hetherington
 ** http://www.neostats.net/
 **
 **  This program is free software; you can redistribute it and/or modify
@@ -43,7 +43,7 @@ Bot *opsb_bot;
 
 /** Copyright info */
 const char *opsb_copyright[] = {
-	"Copyright (c) 1999-2004, NeoStats",
+	"Copyright (c) 1999-2005, NeoStats",
 	"http://www.neostats.net/",
 	NULL
 };
